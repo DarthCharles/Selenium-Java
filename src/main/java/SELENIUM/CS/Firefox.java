18cf6e4842d4838e3deadd9b3d7b386baa62e96d
Erick <erick.sanchez.valenzuela@gmail.com>
2015-05-15 01:50:10 +0200

    Actualizado livejournal

--- src/main/java/SELENIUM/CS/Firefox.java
+++ src/main/java/SELENIUM/CS/Firefox.java
@@ -9,18 +9,19 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class Firefox {
 
 
-	private String baseUrl = "https://mural.ly/";
+	private String baseUrl = "http://www.livejournal.com/";
 
 
 	public void getLogin(WebDriver driver) {
 		  driver.get(baseUrl);
-		    driver.findElement(By.cssSelector("a.login:nth-child(5)")).click();   
-		    driver.findElement(By.name("email")).clear();
-		    driver.findElement(By.name("email")).sendKeys("cds.pruebas@gmail.com");
-		    driver.findElement(By.xpath("//button")).click();
-		    driver.findElement(By.id("Passwd")).clear();
-		    driver.findElement(By.id("Passwd")).sendKeys("calidad123");
-		    driver.findElement(By.id("signIn")).click();
+		    driver.findElement(By.cssSelector("span.i-iconus.s-ljvideo-close")).click();
+		    driver.findElement(By.cssSelector("a[href='http://www.livejournal.com/login.bml']")).click();
+		    driver.findElement(By.id("user")).clear();
+		    driver.findElement(By.id("user")).sendKeys("pruebascalidad");
+		    driver.findElement(By.id("lj_loginwidget_password")).clear();
+		    driver.findElement(By.id("lj_loginwidget_password")).sendKeys("Calidad123");
+		    driver.findElement(By.name("action:login")).click();
+		  
 	}
 	
 	public void getMural(WebDriver driver){
