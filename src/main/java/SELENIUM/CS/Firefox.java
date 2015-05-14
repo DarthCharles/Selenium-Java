651bbc4cc0afa8bf4f65cf32f477aeae44127896
DarthCharles <carlos.darth@gmail.com>
2015-05-15 01:43:54 +0200

    Total reboot

--- src/main/java/SELENIUM/CS/Firefox.java
+++ src/main/java/SELENIUM/CS/Firefox.java
@@ -14,23 +14,9 @@ public class Firefox {
 
 	public void getLogin(WebDriver driver) {
 		  driver.get(baseUrl);
-		    driver.findElement(By.cssSelector("a.login:nth-child(5)")).click();   
-		    driver.findElement(By.name("email")).clear();
-		    driver.findElement(By.name("email")).sendKeys("cds.pruebas@gmail.com");
-		    driver.findElement(By.xpath("//button")).click();
-		    driver.findElement(By.id("Passwd")).clear();
-		    driver.findElement(By.id("Passwd")).sendKeys("calidad123");
-		    driver.findElement(By.id("signIn")).click();
-	}
-	
-	public void getMural(WebDriver driver){
-		this.getLogin(driver);
 
-		    driver.findElement(By.cssSelector("div.mural-thumb-img-container > a > img")).click();
-		    driver.findElement(By.cssSelector("li.black.selected")).click();
-		    driver.findElement(By.cssSelector("ul.filters > li.selected")).click();
 	}
-	
+
 	public void initDriver(WebDriver driver){
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
