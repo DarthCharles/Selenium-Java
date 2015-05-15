591c60e5392c082e21e978a4ffbaf61f0987400f
DarthCharles <carlos.darth@gmail.com>
2015-05-15 03:52:53 +0200

    casos de usos mios de mi terminados

--- src/test/java/editarGrupos/editarGruposAlt.java
+++ src/test/java/editarGrupos/editarGruposAlt.java
@@ -1,5 +1,85 @@
 package editarGrupos;
 
-public class editarGruposAlt {
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
 
+import org.junit.*;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+import SELENIUM.CS.Firefox;
+
+public class editarGruposAlt {
+	private WebDriver driver;
+	private String baseUrl;
+	private boolean acceptNextAlert = true;
+	private StringBuffer verificationErrors = new StringBuffer();
+	private Firefox fox = new Firefox();
+
+	@Before
+	public void setUp() throws Exception {
+		driver = new FirefoxDriver();
+		fox.initDriver(driver);
+	}
+
+	@Test
+	public void testEditGroups() throws Exception {
+		fox.getLogin(driver);
+		Actions action = new Actions(driver);
+		action.moveToElement(driver.findElement(By.cssSelector("li.s-nav-item:nth-child(2) > a:nth-child(1)"))).build().perform();
+		driver.findElement(By.cssSelector(".s-nav-item-editgroups > a:nth-child(1)")).click();
+		new Select(driver.findElement(By.id("list_groups"))).selectByVisibleText("Familia");
+		// ERROR: Caught exception [ERROR: Unsupported command [answerOnNextPrompt | Familia | ]]
+		driver.findElement(By.id("renameGroup")).click();
+		// ERROR: Caught exception [ERROR: Unsupported command [getPrompt |  | ]]
+		driver.findElement(By.xpath("//button[@value='Guardar cambios']")).click();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
+
+	private boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+
+	private boolean isAlertPresent() {
+		try {
+			driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
+
+	private String closeAlertAndGetItsText() {
+		try {
+			Alert alert = driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			acceptNextAlert = true;
+		}
+	}
 }
--- src/test/java/editarPerfil/EditarPerfilAlt.java
+++ src/test/java/editarPerfil/EditarPerfilAlt.java
@@ -1,5 +1,94 @@
 package editarPerfil;
 
-public class EditarPerfilAlt {
 
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import SELENIUM.CS.Firefox;
+
+public class EditarPerfilAlt {
+	private WebDriver driver;
+	private String baseUrl;
+	private boolean acceptNextAlert = true;
+	private StringBuffer verificationErrors = new StringBuffer();
+	private Firefox fox = new Firefox();
+
+
+	@Before
+	public void setUp() throws Exception {
+		driver = new FirefoxDriver();
+		fox.initDriver(driver);
+	}
+
+	@Test
+	public void testEditarPerfil() throws Exception {
+		fox.getLogin(driver);
+		driver.findElement(By.xpath("//html[@id='js']/body/div[4]/header/div/nav/ul[2]/li/a/span")).click();
+		driver.findElement(By.cssSelector(".w-cs-menu--primary")).click();
+		driver.findElement(By.linkText("Account")).click();
+	    driver.findElement(By.linkText("Edit profile")).click();
+	    driver.findElement(By.name("name")).clear();
+	    driver.findElement(By.name("name")).sendKeys("");
+	    driver.findElement(By.name("year")).clear();
+	    driver.findElement(By.name("year")).sendKeys("");
+	    driver.findElement(By.cssSelector("textarea[name=\"interests\"]")).clear();
+	    driver.findElement(By.cssSelector("textarea[name=\"interests\"]")).sendKeys("");
+	    driver.findElement(By.id("written_state")).clear();
+	    driver.findElement(By.id("written_state")).sendKeys("");
+	    driver.findElement(By.id("city")).clear();
+	    driver.findElement(By.id("city")).sendKeys("");
+	    driver.findElement(By.cssSelector("button.b-flatbutton")).click();
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		//driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
+
+	private boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+
+	private boolean isAlertPresent() {
+		try {
+			driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
+
+	private String closeAlertAndGetItsText() {
+		try {
+			Alert alert = driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			acceptNextAlert = true;
+		}
+	}
 }
