6f499aac8117c1ad875ff49b8a9ae02256c68e4c
Erick <erick.sanchez.valenzuela@gmail.com>
2015-05-21 22:12:55 +0200

    SauceTests cambiado

--- src/main/java/sauceLabs/SauceTests.java
+++ src/main/java/sauceLabs/SauceTests.java
@@ -11,23 +11,31 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class SauceTests {
-	
-	public WebDriver WindowsFirefoxDriver() throws MalformedURLException{
-		DesiredCapabilities capabilities = new DesiredCapabilities().firefox();
-		capabilities.setCapability("version", "17");
-		capabilities.setCapability("platform", Platform.XP);
-		capabilities.setCapability("name", "Prueba CS 2015");
-		return new RemoteWebDriver(new URL("http://DarthCharles:fddc5aa3-5c0e-47a1-a941-48cc635665fe@ondemand.saucelabs.com:80/wd/hub"), 
-				capabilities);
+
+	DesiredCapabilities capabilities;
+	URL url;
+
+	public SauceTests(){
+		try{
+			url = new URL("http://DarthCharles:fddc5aa3-5c0e-47a1-a941-48cc635665fe@ondemand.saucelabs.com:80/wd/hub");
+		}catch(MalformedURLException e){
+			System.out.println(e.getCause());
+		}
 	}
-	
+	public void setCapabilities(DesiredCapabilities navegador, String version, Platform platform){
+		capabilities = navegador;
+		capabilities.setCapability("version", version);
+		capabilities.setCapability("platform", platform);
+	}
+
+	public WebDriver WindowsFirefoxDriver() throws MalformedURLException{
+		this.setCapabilities(DesiredCapabilities.firefox(), "17", Platform.XP);
+		return new RemoteWebDriver(url, capabilities);
+	}
+
 	public WebDriver MacSafariDriver() throws MalformedURLException{
-		DesiredCapabilities capabilities = new DesiredCapabilities().safari();
-		capabilities.setCapability("version", "44.0.2403.9");
-		capabilities.setCapability("platform", Platform.MAC);
-		capabilities.setCapability("name", "Prueba CS 2015");
-		return new RemoteWebDriver(new URL("http://DarthCharles:fddc5aa3-5c0e-47a1-a941-48cc635665fe@ondemand.saucelabs.com:80/wd/hub"), 
-				capabilities);
+		this.setCapabilities(DesiredCapabilities.safari(), "44.0.2403.9", Platform.MAC);
+		return new RemoteWebDriver(url, capabilities);
 	}
 
 }
