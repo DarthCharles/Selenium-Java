f7cf8d2199aea7599afb839f5b15721d23b459a8
Erick <erick.sanchez.valenzuela@gmail.com>
2015-05-21 23:16:17 +0200

    Cambiadas versiones de mac

--- src/main/java/sauceLabs/SauceTests.java
+++ src/main/java/sauceLabs/SauceTests.java
@@ -29,8 +29,7 @@ public class SauceTests {
 		capabilities = navegador;
 		capabilities.setCapability("version", version);
 		capabilities.setCapability("platform", platform);
-        capabilities.setCapability("name", this.name );
-
+		capabilities.setCapability("name", this.name );
 	}
 
 	public WebDriver WindowsFirefoxDriver() throws MalformedURLException{
@@ -39,7 +38,7 @@ public class SauceTests {
 	}
 
 	public WebDriver MacSafariDriver() throws MalformedURLException{
-		this.setCapabilities(DesiredCapabilities.safari(), "44.0.2403.9", Platform.MAC, this.name);
+		this.setCapabilities(DesiredCapabilities.safari(), "8", Platform.YOSEMITE, this.name);
 		return new RemoteWebDriver(url, capabilities);
 	}
 
